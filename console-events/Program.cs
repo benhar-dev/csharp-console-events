53dfff93470c533b612dc1c981264daf23effad6
Ben Harrison <53424421+benhar-dev@users.noreply.github.com>
2021-08-31 11:31:11 +1000

    using struct

--- console-events/Program.cs
+++ console-events/Program.cs
@@ -2,6 +2,7 @@
 using TwinCAT.Ads;
 using System.Text;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 [Flags]
 public enum ControlMask
@@ -13,6 +14,31 @@ public enum ControlMask
     ADSLOG_MSGTYPE_MSGBOX = 32,
     ADSLOG_MSGTYPE_RESOURCE = 64,
     ADSLOG_MSGTYPE_STRING = 128
+}
+
+[StructLayout(LayoutKind.Explicit, Size = 36, Pack = 1)]
+public struct AdsLogNotificationHeader
+{
+    [MarshalAs(UnmanagedType.U8)]
+    [FieldOffset(0)]
+    public Int64 nTimeStamp;
+
+    [MarshalAs(UnmanagedType.U4)]
+    [FieldOffset(8)]
+    public ControlMask dwMsgCtrl;
+
+    [MarshalAs(UnmanagedType.U4)]
+    [FieldOffset(12)]
+    public uint nServerPort;
+
+    [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 16)]
+    [FieldOffset(16)]
+    public string sDeviceName;
+
+    [MarshalAs(UnmanagedType.U4)]
+    [FieldOffset(32)]
+    public uint cbMsgSize;
+
 }
 
 namespace console_events
@@ -35,7 +61,7 @@ namespace console_events
                 client.AdsNotification += Client_AdsNotification;
 
                 // Connect to target
-                client.Connect(AmsNetId.Local, 110);
+                client.Connect(AmsNetId.Local, 100);
                 uint notificationHandle = 0;
 
                 try
@@ -58,41 +84,63 @@ namespace console_events
 
             var dataArray = e.Data.Span.ToArray();
 
-            //Console.WriteLine("Hex: {0:X}", dataArray);
             Console.WriteLine("Notification as Hex");
             Console.WriteLine("-------------------");
             Console.WriteLine(string.Join(", ", dataArray.Select(b => b.ToString("X2"))));
             Console.WriteLine("");
 
-            // write the date time
-            long fileTimeRaw = BitConverter.ToInt64(dataArray, 0);
+            AdsLogNotificationHeader header = ByteArrayToAdsLogNotificationHeader(dataArray);
+
             Console.WriteLine("Timestamp");
-            Console.WriteLine("---------");
-            Console.WriteLine(DateTime.FromFileTime(fileTimeRaw));
+            Console.WriteLine(DateTime.FromFileTime(header.nTimeStamp));
             Console.WriteLine("");
 
-            // write the control mask
-            Console.WriteLine("Control Mask");
-            Console.WriteLine("------------");
-            Console.WriteLine("{1:G}", dataArray[8], (ControlMask)dataArray[8]);
+            Console.WriteLine("Control mask");
+            Console.WriteLine("{0:G}", header.dwMsgCtrl);
             Console.WriteLine("");
 
-            // write server port
-            ulong serverPort = BitConverter.ToUInt64(dataArray, 9);
-            Console.WriteLine("Server Port");
-            Console.WriteLine("---------");
-            Console.WriteLine(serverPort.ToString());
+            Console.WriteLine("Server port");
+            Console.WriteLine(header.nServerPort.ToString());
             Console.WriteLine("");
 
-            // write the message
-            Console.WriteLine("Message");
-            Console.WriteLine("-------");
-            string message = Encoding.ASCII.GetString(dataArray);
-            Console.WriteLine(message.Substring(16));
+            Console.WriteLine("Device name");
+            Console.WriteLine(header.sDeviceName);
             Console.WriteLine("");
 
+            Console.WriteLine("Message size");
+            Console.WriteLine(header.cbMsgSize.ToString());
+            Console.WriteLine("");
+
+            string message = ByteArrayToAdsLogNotificationMessage(dataArray);
+
+            Console.WriteLine("Message");   
+            Console.WriteLine(message);
+            Console.WriteLine("");
 
         }
-        
+
+        static AdsLogNotificationHeader ByteArrayToAdsLogNotificationHeader(byte[] bytes)
+        {
+            GCHandle handle = GCHandle.Alloc(bytes, GCHandleType.Pinned);
+            AdsLogNotificationHeader adsLogNotificationHeader;
+
+            try
+            {
+                adsLogNotificationHeader = (AdsLogNotificationHeader)Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(AdsLogNotificationHeader));
+            }
+            finally
+            {
+                handle.Free();
+            }
+
+            return adsLogNotificationHeader;
+        }
+
+        static string ByteArrayToAdsLogNotificationMessage(byte[] bytes)
+        {
+            int size = System.Runtime.InteropServices.Marshal.SizeOf(typeof(AdsLogNotificationHeader));
+            return Encoding.ASCII.GetString(bytes).Substring(size);
+        }
+
     }
 }
